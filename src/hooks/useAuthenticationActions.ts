863b322e4be1c1b3441750eb9b16944dce5327ed
Yoel Augustan <yoelagustan@gmail.com>
2025-06-11 23:33:11 +0700

    create auth hook

---
+++ src/hooks/useAuthenticationActions.ts
@@ -0,0 +1,71 @@
+'use client'
+
+import { useState } from 'react';
+import { createClient } from '@/utils/supabase/client'
+import { useRouter } from 'next/navigation';
+
+
+export function useAuthenticationActions() {
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+    const router = useRouter();
+    const supabase = createClient()
+
+    const signOut = async () => {
+        await supabase.auth.signOut();
+        router.push('/');
+    };
+
+    const login = async (email: string, password: string) => {
+        setLoading(true);
+        setError(null);
+        const { error } = await supabase.auth.signInWithPassword({ email, password });
+        setLoading(false);
+
+        if (error) {
+            setError(error.message);
+        } else {
+            router.push('/landing');
+        }
+    };
+
+    const signUp = async (email: string, password: string) => {
+        console.log('Signing up...');
+        setLoading(true);
+        setError(null);
+
+        const { error } = await supabase.auth.signUp({ email, password, });
+
+        setLoading(false);
+
+        if (error) {
+            setError(error.message);
+        } else {
+            router.push('/landing');
+        }
+    };
+
+    const signInWithGoogle = async () => {
+        console.log('Signing in with Google...');
+        const { error } = await supabase.auth.signInWithOAuth({
+            provider: 'google',
+        });
+
+
+        if (error) {
+            console.error('Google sign-in error:', error.message);
+        } else {
+            router.push('/landing');
+        }
+    };
+
+
+    return {
+        loading,
+        error,
+        signOut,
+        login,
+        signUp,
+        signInWithGoogle
+    }
+}
